45db634275ae3006ad09b712fb6eddc98d134530
xwatson <xwatson@foxmail.com>
2018-11-06 19:04:34 +0800

    add urls

--- config/url.js
+++ config/url.js
@@ -3,5 +3,11 @@ module.exports = {
         "Index": "https://www.tmall.com",
         "Login": "https://login.m.taobao.com/login.htm",
         "XiaoMiTV4S50": "https://detail.m.tmall.com/item.htm?id=574693149048&skuId=3763317276188"
+    },
+    "JD": {
+        "XiaoMiTV4S50": "https://item.m.jd.com/product/7365058.html"
+    },
+    "SuNing": {
+        "XiaoMiTV4S50": "https://m.suning.com/product/0000000000/10567920410.html?safp=d488778a.13701.0.855dfe8be2"
     }
 }
