e2270652e84d9207ffb65d20591d83f66a47b82e
xwatson <xwatson@foxmail.com>
2018-11-06 18:05:10 +0800

    add comfig

---
+++ config/url.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "Tmall": {
+        "Index": "https://www.tmall.com",
+        "Login": "https://login.m.taobao.com/login.htm",
+        "XiaoMiTV4S50": "https://detail.m.tmall.com/item.htm?id=574693149048&skuId=3763317276188"
+    }
+}
